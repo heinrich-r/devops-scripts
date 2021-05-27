a9c0badf9be73c936f617a769a13cf03870994d1
SuperUnknown <shhh@superunknown.gg>
2021-05-27 22:40:45 +0200

    Additional step for git

--- virtualbox/1_sudo_user.sh
+++ virtualbox/1_sudo_user.sh
@@ -1,10 +1,3 @@
-#!/bin/bash
-if [ -z "$1" ]
-then
-    echo "\$1 is empty (requires user)"
-else
-    echo "Adding user [" + $1 + "] to Sudo"
-    su -i
-    sudo usermod -aG sudo $1
-    shutdown -r now
-fi
+su
+sudo usermod -aG sudo {username}
+shutdown -r now
